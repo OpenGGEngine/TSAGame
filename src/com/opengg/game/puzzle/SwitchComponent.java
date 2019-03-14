14013e268332a7c13a73e96c734e7f33de46453c
warren <rlaterson@gmail.com>
2019-03-14 18:23:24 -0400

    Doors and Switches

---
+++ src/com/opengg/game/puzzle/SwitchComponent.java
@@ -0,0 +1,25 @@
+package com.opengg.game.puzzle;
+
+import com.opengg.core.model.Model;
+import com.opengg.core.world.components.Component;
+import com.opengg.core.world.components.ModelComponent;
+
+import java.util.HashSet;
+
+public class SwitchComponent extends Component {
+    public static HashSet<String> triggered = new HashSet<>();
+    private boolean state = false;
+    private ModelComponent model;
+    public String name;
+    
+    public SwitchComponent(String name,Model m){
+        this.name = name;
+        model = new ModelComponent(m);
+        this.attach(model);
+    }
+    public void toggle(){
+        state = !state;
+        if(state) triggered.add(name);
+        else triggered.remove(name);
+    }
+}
